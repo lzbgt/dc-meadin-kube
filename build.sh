de804721e6b8cc6147dfd8c3d04239504cd86329
Bruce.Lu <lzbgt@126.com>
2017-12-04 11:56:39 +0800

    init

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ proj_base=/home/hadoop/work/dc-parent
 docker_registry=node1:30400
 kube_dir=/home/hadoop/work/k8s/dc-meadin
 
-kubectl delete -f $kube_dir
+#kubectl delete -f $kube_dir
 
 declare -a dir_modules=("dc-common-module" "dc-common-druid" "dc-common-business" "dc-mongodb-api" "dc-external-api-api" "dc-redis-api" "dc-solr-api" "dc-sso-api" "dc-statistics-api" "dc-reptile-api")
 
@@ -16,7 +16,7 @@ do
   mvn clean install
 done
 
-declare -a dir_services=("dc-redis" "dc-mongodb" "dc-sso" "dc-customer-main" "dc-customer-portrayal" "dc-eureka-server" "dc-customer-sentiment")
+declare -a dir_services=("dc-redis" "dc-mongodb" "dc-sso" "dc-solr" "dc-customer-main" "dc-customer-portrayal" "dc-eureka-server" "dc-customer-sentiment")
 for i in "${dir_services[@]}"
 do
   cd $proj_base/$i
@@ -25,4 +25,4 @@ do
   sudo docker push $docker_registry/$i:$ver
 done
 
-kubectl apply -f $kube_dir
+#kubectl apply -f $kube_dir
